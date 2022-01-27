f4020d13f082bf7db44a800050b3bf01045105ba
lazzzaroni <iamnotlazzaroni@gmail.com>
2022-01-27 13:55:26 +0100

    handleError

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -34,8 +34,30 @@ export class HeroService {
     return of(hero);
   }
 
-  /** Log a HeroService message with the MessageService */
+  /**
+   * Log a HeroService message with the MessageService
+   */
   private log(message: string) {
     this.messageService.add(`HeroService: ${message}`);
+  }
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   *
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 }
